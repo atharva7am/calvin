5b4fed842469c726a69eeb3e11d63ad8f009a316
Atharva Mete <a.mete.2416@gmail.com>
2024-01-17 00:12:21 -0500

    added hydra support for evals

--- calvin_models/calvin_agent/evaluation/evaluate_policy.py
+++ calvin_models/calvin_agent/evaluation/evaluate_policy.py
@@ -42,12 +42,12 @@ from gpt_prior_global import GPT, GPTConfig
 
 logger = logging.getLogger(__name__)
 
-EP_LEN = 192
-NUM_SEQUENCES = 2
-ACTION_HORIZON = 32
-DEFAULT_LOG = "/home/amete7/calvin/calvin_models/calvin_agent/evaluation/eval_log"
-SAVE_ROLLOUT_VIDEO = False
-EXP_NAME = "calvin_agent"
+# EP_LEN = 192
+# NUM_SEQUENCES = 2
+# ACTION_HORIZON = 32
+# DEFAULT_LOG = "/home/amete7/calvin/calvin_models/calvin_agent/evaluation/eval_log"
+# SAVE_ROLLOUT_VIDEO = False
+# EXP_NAME = "calvin_agent"
 
 def get_epoch(checkpoint):
     if "=" not in checkpoint.stem:
@@ -56,29 +56,29 @@ def get_epoch(checkpoint):
 
 
 class CustomModel(CalvinBaseModel):
-    def __init__(self,config_path):
+    def __init__(self,exp_cfg):
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.max_indices = 8
         self.dummy_index = 1001
+        self.prior_type = exp_cfg.prior_type
 
         model_name = "openai/clip-vit-base-patch32"
         self.processor = CLIPProcessor.from_pretrained(model_name)
         self.clip_model = CLIPModel.from_pretrained(model_name)
         self.clip_model = self.clip_model.to(self.device)
 
-        model_cfg = OmegaConf.load(config_path)
-        model_ckpt = model_cfg.paths.model_weights_path
-        priot_ckpt = model_cfg.paths.prior_weights_path
+        if self.prior_type == 'gpt':
+            gpt_priot_ckpt = exp_cfg.paths.gpt_prior_weights_path
+            gpt_config = GPTConfig(vocab_size=exp_cfg.gpt_prior.vocab_size, block_size=exp_cfg.gpt_prior.block_size, output_dim=exp_cfg.gpt_prior.output_dim, discrete_input=True)
+            self.gpt_prior_model = GPT(gpt_config).to(self.device)
+            state_dict = torch.load(gpt_priot_ckpt, map_location='cuda')
+            self.gpt_prior_model.load_state_dict(state_dict)
+            self.gpt_prior_model = self.gpt_prior_model.to(self.device)
+            self.gpt_prior_model.eval()
+            print('gpt_prior_model_loaded')
 
-        gpt_config = GPTConfig(vocab_size=model_cfg.prior.vocab_size, block_size=model_cfg.prior.block_size, output_dim=model_cfg.prior.output_dim, discrete_input=True)
-        self.gpt_prior_model = GPT(gpt_config).to(self.device)
-        state_dict = torch.load(priot_ckpt, map_location='cuda')
-        self.gpt_prior_model.load_state_dict(state_dict)
-        self.gpt_prior_model = self.gpt_prior_model.to(self.device)
-        self.gpt_prior_model.eval()
-        print('gpt_prior_model_loaded')
-
-        self.decoder = SkillAutoEncoder(model_cfg.model)
+        model_ckpt = exp_cfg.paths.model_weights_path
+        self.decoder = SkillAutoEncoder(exp_cfg.model)
         state_dict = torch.load(model_ckpt, map_location='cuda')
         self.decoder.load_state_dict(state_dict)
         self.decoder = self.decoder.to(self.device)
@@ -106,14 +106,15 @@ class CustomModel(CalvinBaseModel):
         return language_features
 
     def get_indices(self, attach_emb):
-        indices = [self.dummy_index]
-        for _ in range(self.max_indices):
-            x = torch.tensor(indices).unsqueeze(0).to(self.device)
-            with torch.no_grad():
-                logits = self.gpt_prior_model(x, None, attach_emb, [0,0])
-            next_token = logits[0,-1,:].argmax().item()
-            indices.append(next_token)
-        return torch.tensor(indices[1:]).unsqueeze(0).to(self.device)
+        if self.prior_type == 'gpt':
+            indices = [self.dummy_index]
+            for _ in range(self.max_indices):
+                x = torch.tensor(indices).unsqueeze(0).to(self.device)
+                with torch.no_grad():
+                    logits = self.gpt_prior_model(x, None, attach_emb, [0,0])
+                next_token = logits[0,-1,:].argmax().item()
+                indices.append(next_token)
+            return torch.tensor(indices[1:]).unsqueeze(0).to(self.device)
 
     def step(self, obs, goal):
         """
@@ -146,7 +147,7 @@ class CustomModel(CalvinBaseModel):
         action[:,-1] = (((action[:,-1] >= 0) * 2) - 1).astype(int)
         return action
 
-def evaluate_policy(model, env, epoch=0, eval_log_dir=DEFAULT_LOG, debug=False, create_plan_tsne=False):
+def evaluate_policy(model, env, exp_cfg, epoch=0, debug=False, create_plan_tsne=False):
     """
     Run this function to evaluate a model on the CALVIN challenge.
 
@@ -154,21 +155,21 @@ def evaluate_policy(model, env, epoch=0, eval_log_dir=DEFAULT_LOG, debug=False, 
         model: Must implement methods of CalvinBaseModel.
         env: (Wrapped) calvin env.
         epoch:
-        eval_log_dir: Path where to log evaluation results. If None, logs to /tmp/evaluation/
         debug: If True, show camera view and debug info.
         create_plan_tsne: Collect data for TSNE plots of latent plans (does not work for your custom model)
 
     Returns:
         Dictionary with results
     """
-    conf_dir = Path(__file__).absolute().parents[2] / "conf"
+    conf_dir = Path(__file__).absolute().parents[2] / "../../../../../conf"
     task_cfg = OmegaConf.load(conf_dir / "callbacks/rollout/tasks/new_playtable_tasks.yaml")
     task_oracle = hydra.utils.instantiate(task_cfg)
     val_annotations = OmegaConf.load(conf_dir / "annotations/new_playtable_validation.yaml")
 
+    eval_log_dir = exp_cfg.paths.eval_log_dir
     eval_log_dir = get_log_dir(eval_log_dir)
 
-    eval_sequences = get_sequences(NUM_SEQUENCES)
+    eval_sequences = get_sequences(exp_cfg.eval.num_sequences)
 
     results = []
     plans = defaultdict(list)
@@ -177,7 +178,7 @@ def evaluate_policy(model, env, epoch=0, eval_log_dir=DEFAULT_LOG, debug=False, 
         eval_sequences = tqdm(eval_sequences, position=0, leave=True)
 
     for initial_state, eval_sequence in eval_sequences:
-        result = evaluate_sequence(env, model, task_oracle, initial_state, eval_sequence, val_annotations, plans, debug)
+        result = evaluate_sequence(env, model, task_oracle, initial_state, eval_sequence, val_annotations, plans, debug, exp_cfg)
         results.append(result)
         if not debug:
             eval_sequences.set_description(
@@ -186,12 +187,12 @@ def evaluate_policy(model, env, epoch=0, eval_log_dir=DEFAULT_LOG, debug=False, 
 
     if create_plan_tsne:
         create_tsne(plans, eval_log_dir, epoch)
-    print_and_save(results, eval_sequences, eval_log_dir, epoch, exp=EXP_NAME)
+    print_and_save(results, eval_sequences, eval_log_dir, epoch, exp=exp_cfg.exp_name)
 
     return results
 
 
-def evaluate_sequence(env, model, task_checker, initial_state, eval_sequence, val_annotations, plans, debug):
+def evaluate_sequence(env, model, task_checker, initial_state, eval_sequence, val_annotations, plans, debug, exp_cfg):
     """
     Evaluates a sequence of language instructions.
     """
@@ -206,7 +207,7 @@ def evaluate_sequence(env, model, task_checker, initial_state, eval_sequence, va
         print(f"Evaluating sequence: {' -> '.join(eval_sequence)}")
         print("Subtask: ", end="")
     for subtask in eval_sequence:
-        success = rollout(env, model, task_checker, subtask, val_annotations, plans, debug)
+        success = rollout(env, model, task_checker, subtask, val_annotations, plans, debug, exp_cfg)
         if success:
             success_counter += 1
         else:
@@ -214,7 +215,7 @@ def evaluate_sequence(env, model, task_checker, initial_state, eval_sequence, va
     return success_counter
 
 
-def rollout(env, model, task_oracle, subtask, val_annotations, plans, debug):
+def rollout(env, model, task_oracle, subtask, val_annotations, plans, debug, exp_cfg):
     """
     Run the actual rollout on one subtask (which is one natural language instruction).
     """
@@ -226,7 +227,7 @@ def rollout(env, model, task_oracle, subtask, val_annotations, plans, debug):
     lang_annotation = val_annotations[subtask][0]
     model.reset(lang_annotation)
     start_info = env.get_info()
-    save_video = SAVE_ROLLOUT_VIDEO
+    save_video = exp_cfg.eval.save_video
     if save_video:
         output_video_path = f'rollout_{subtask}.mp4'
         frame_size = (200,200)
@@ -234,9 +235,9 @@ def rollout(env, model, task_oracle, subtask, val_annotations, plans, debug):
         fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Use 'mp4v' or 'xvid' for MP4, 'MJPG' for AVI
         video_writer = cv2.VideoWriter(output_video_path, fourcc, fps, frame_size)
 
-    for step in range(EP_LEN//ACTION_HORIZON):
+    for step in range(exp_cfg.eval.episode_length//exp_cfg.eval.action_horizon):
         actions = model.step(obs, lang_annotation)
-        for timestep in range(ACTION_HORIZON):
+        for timestep in range(exp_cfg.eval.action_horizon):
             action_to_take = actions[timestep].copy()
             action_to_take = ((action_to_take[0],action_to_take[1],action_to_take[2]),(action_to_take[3],action_to_take[4],action_to_take[5]),(action_to_take[-1],))
             obs, _, _, current_info = env.step(action_to_take)
@@ -257,94 +258,93 @@ def rollout(env, model, task_oracle, subtask, val_annotations, plans, debug):
         print(colored("fail", "red"), end=" ")
     return False
 
-
-def main(env_cfg):
+@hydra.main(config_path="../../../calvin_env/conf/", config_name="diff_skill_config")
+def main(exp_cfg):
     seed_everything(0, workers=True)  # type:ignore
-    parser = argparse.ArgumentParser(description="Evaluate a trained model on multistep sequences with language goals.")
-    parser.add_argument("--dataset_path", type=str, help="Path to the dataset root directory.")
+    # parser = argparse.ArgumentParser(description="Evaluate a trained model on multistep sequences with language goals.")
+    # parser.add_argument("--dataset_path", type=str, help="Path to the dataset root directory.")
 
-    # arguments for loading default model
-    parser.add_argument(
-        "--train_folder", type=str, help="If calvin_agent was used to train, specify path to the log dir."
-    )
-    parser.add_argument(
-        "--checkpoints",
-        type=str,
-        default=None,
-        help="Comma separated list of epochs for which checkpoints will be loaded",
-    )
-    parser.add_argument(
-        "--checkpoint",
-        type=str,
-        default=None,
-        help="Path of the checkpoint",
-    )
-    parser.add_argument(
-        "--last_k_checkpoints",
-        type=int,
-        help="Specify the number of checkpoints you want to evaluate (starting from last). Only used for calvin_agent.",
-    )
+    # # arguments for loading default model
+    # parser.add_argument(
+    #     "--train_folder", type=str, help="If calvin_agent was used to train, specify path to the log dir."
+    # )
+    # parser.add_argument(
+    #     "--checkpoints",
+    #     type=str,
+    #     default=None,
+    #     help="Comma separated list of epochs for which checkpoints will be loaded",
+    # )
+    # parser.add_argument(
+    #     "--checkpoint",
+    #     type=str,
+    #     default=None,
+    #     help="Path of the checkpoint",
+    # )
+    # parser.add_argument(
+    #     "--last_k_checkpoints",
+    #     type=int,
+    #     help="Specify the number of checkpoints you want to evaluate (starting from last). Only used for calvin_agent.",
+    # )
 
-    # arguments for loading custom model or custom language embeddings
-    parser.add_argument(
-        "--custom_model", action="store_true", help="Use this option to evaluate a custom model architecture."
-    )
+    # # arguments for loading custom model or custom language embeddings
+    # parser.add_argument(
+    #     "--custom_model", action="store_true", help="Use this option to evaluate a custom model architecture."
+    # )
 
-    parser.add_argument("--debug", action="store_true", help="Print debug info and visualize environment.")
+    # parser.add_argument("--debug", action="store_true", help="Print debug info and visualize environment.")
 
-    parser.add_argument("--eval_log_dir", default=None, type=str, help="Where to log the evaluation results.")
+    # parser.add_argument("--eval_log_dir", default=None, type=str, help="Where to log the evaluation results.")
 
-    parser.add_argument("--device", default=0, type=int, help="CUDA device")
+    # parser.add_argument("--device", default=0, type=int, help="CUDA device")
 
-    parser.add_argument("--config_path", default="/home/amete7/calvin/calvin_models/conf/diff_skill_config.yaml", type=str, help="Path to model config file.")
-    args = parser.parse_args()
-
+    # parser.add_argument("--config_path", default="/home/amete7/calvin/calvin_models/conf/diff_skill_config.yaml", type=str, help="Path to model config file.")
+    # args = parser.parse_args()
     # evaluate a custom model
-    if args.custom_model:
-        model = CustomModel(args.config_path)
-        env = hydra.utils.instantiate(env_cfg.env)
-        evaluate_policy(model, env, debug=args.debug)
-    else:
-        assert "train_folder" in args
+    # if args.custom_model:
+    model = CustomModel(exp_cfg)
+    env = hydra.utils.instantiate(exp_cfg.env)
+    evaluate_policy(model, env, exp_cfg, debug=False)
+    # else:
+    #     assert "train_folder" in args
 
-        checkpoints = []
-        if args.checkpoints is None and args.last_k_checkpoints is None and args.checkpoint is None:
-            print("Evaluating model with last checkpoint.")
-            checkpoints = [get_last_checkpoint(Path(args.train_folder))]
-        elif args.checkpoints is not None:
-            print(f"Evaluating model with checkpoints {args.checkpoints}.")
-            checkpoints = get_checkpoints_for_epochs(Path(args.train_folder), args.checkpoints)
-        elif args.checkpoints is None and args.last_k_checkpoints is not None:
-            print(f"Evaluating model with last {args.last_k_checkpoints} checkpoints.")
-            checkpoints = get_all_checkpoints(Path(args.train_folder))[-args.last_k_checkpoints :]
-        elif args.checkpoint is not None:
-            checkpoints = [Path(args.checkpoint)]
+    #     checkpoints = []
+    #     if args.checkpoints is None and args.last_k_checkpoints is None and args.checkpoint is None:
+    #         print("Evaluating model with last checkpoint.")
+    #         checkpoints = [get_last_checkpoint(Path(args.train_folder))]
+    #     elif args.checkpoints is not None:
+    #         print(f"Evaluating model with checkpoints {args.checkpoints}.")
+    #         checkpoints = get_checkpoints_for_epochs(Path(args.train_folder), args.checkpoints)
+    #     elif args.checkpoints is None and args.last_k_checkpoints is not None:
+    #         print(f"Evaluating model with last {args.last_k_checkpoints} checkpoints.")
+    #         checkpoints = get_all_checkpoints(Path(args.train_folder))[-args.last_k_checkpoints :]
+    #     elif args.checkpoint is not None:
+    #         checkpoints = [Path(args.checkpoint)]
 
-        env = None
-        for checkpoint in checkpoints:
-            epoch = get_epoch(checkpoint)
-            model, env, _ = get_default_model_and_env(
-                args.train_folder,
-                args.dataset_path,
-                checkpoint,
-                env=env,
-                device_id=args.device,
-            )
-            evaluate_policy(model, env, epoch, eval_log_dir=args.eval_log_dir, debug=args.debug, create_plan_tsne=True)
+    #     env = None
+    #     for checkpoint in checkpoints:
+    #         epoch = get_epoch(checkpoint)
+    #         model, env, _ = get_default_model_and_env(
+    #             args.train_folder,
+    #             args.dataset_path,
+    #             checkpoint,
+    #             env=env,
+    #             device_id=args.device,
+    #         )
+    #         evaluate_policy(model, env, epoch, eval_log_dir=args.eval_log_dir, debug=args.debug, create_plan_tsne=True)
 
 
 if __name__ == "__main__":
     # main()
-    with initialize(config_path="../../../calvin_env/conf/"):
-        # print("config path:")
-        env_cfg = compose(config_name="config_data_collection.yaml", overrides=["cameras=static_and_gripper"])
-        env_cfg.env["use_egl"] = False
-        env_cfg.env["show_gui"] = False
-        env_cfg.env["use_vr"] = False
-        env_cfg.env["use_scene_info"] = True
+    # with initialize(config_path="../../../calvin_env/conf/"):
+    #     # print("config path:")
+    #     env_cfg = compose(config_name="config_data_collection.yaml", overrides=["cameras=static_and_gripper"])
+    #     env_cfg.env["use_egl"] = False
+    #     env_cfg.env["show_gui"] = False
+    #     env_cfg.env["use_vr"] = False
+    #     env_cfg.env["use_scene_info"] = True
     try:
         # Your code that may raise an exception here
-        main(env_cfg)
+        main()
     except Exception as e:
         # Handle the exception or simply do nothing to suppress the local variable display
         traceback.print_exc()
